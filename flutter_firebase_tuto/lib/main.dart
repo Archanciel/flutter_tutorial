d6dddf6a793003a403e2166f33c0a67ad7c51cf0
Archanciel <jp.schnyder@gmail.com>
2022-05-30 21:15:30 +0200

    init commit

---
+++ flutter_firebase_tuto/lib/main.dart
@@ -0,0 +1,159 @@
+// ignore_for_file: avoid_print
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'add_movie_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      title: 'Flutter Firebase',
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Firebase'),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return const AddPage();
+                  },
+                  fullscreenDialog: true,
+                ),
+              );
+            },
+            icon: const Icon(Icons.add)),
+      ),
+      body: const SingleChildScrollView(
+        child: MoviesInformation(),
+      ),
+    );
+  }
+}
+
+class MoviesInformation extends StatefulWidget {
+  const MoviesInformation({Key? key}) : super(key: key);
+  @override
+  _MoviesInformationState createState() => _MoviesInformationState();
+}
+
+class _MoviesInformationState extends State<MoviesInformation> {
+  final Stream<QuerySnapshot> _moviesStream =
+      FirebaseFirestore.instance.collection('Movies').snapshots();
+
+  void addLike(String docID, int likes) {
+    var newLiks = likes + 1;
+    try {
+      FirebaseFirestore.instance.collection('Movies').doc(docID).update({
+        'likes': newLiks,
+      }).then((value) {
+        print('Base Firestore à jour');
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _moviesStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: Text("Loading"),
+          );
+        }
+
+        return Column(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> movie =
+                document.data()! as Map<String, dynamic>;
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: Image.network(movie['poster']),
+                  ),
+                  SizedBox(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            movie['name'],
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const Text('Année de production'),
+                          Text(movie['year'].toString()),
+                          Row(
+                            children: [
+                              for (final categorie in movie['categories'])
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  child: Chip(
+                                    backgroundColor: Colors.lightBlue,
+                                    label: Text(
+                                      categorie,
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                padding: EdgeInsets.zero,
+                                constraints: const BoxConstraints(),
+                                iconSize: 20,
+                                onPressed: () {
+                                  addLike(document.id, movie['likes']);
+                                },
+                                icon: const Icon(Icons.favorite),
+                              ),
+                              const SizedBox(width: 10),
+                              Text(movie['likes'].toString() + ' likes'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
